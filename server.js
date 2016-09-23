ff75e4821a1ff37edf78c8cb5090f9a01b9b8c03
sumitevs <sumitevs@gmail.com>
2016-09-23 10:08:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,17 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/article-one',function(req,res){
+    
+});
+
+app.get('/article-two',function(req,res){
+    
+});
+
+app.get('/article-three',function(req,res){
+    
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
