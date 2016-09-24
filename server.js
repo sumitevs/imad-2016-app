c9d5e3b7756aaf11efdfd43af1e3d0181faaef7e
sumitevs <sumitevs@gmail.com>
2016-09-24 11:53:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,28 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne ={
+var articles = {
+    'article-one':{
     title: 'Article one |sumit',
     heading: 'Article one',
     date: 'Sep 5,2016',
-    content: `<p>this is conent for article.
+    content: `<p>this is conent for article one.
     </p>
-    `
+    `},
+     'article-two':{
+    title: 'Article one |sumit',
+    heading: 'Article one',
+    date: 'Sep 5,2016',
+    content: `<p>this is conent for article two.
+    </p>
+    `},
+     'article-three':{
+    title: 'Article one |sumit',
+    heading: 'Article one',
+    date: 'Sep 5,2016',
+    content: `<p>this is conent for article three .
+    </p>
+    `}
 };
 
 function createTemplate(data){
@@ -64,17 +79,13 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article-one',function(req,res){
- res.send(createTemplate(articleOne));   
+app.get('/:articleName',function(req,res){
+    //articleName = article-one
+    var articleName = req.params.articleName;
+ res.send(createTemplate(articles[articleName]));   
 });
 
-app.get('/article-two',function(req,res){
-    
-});
 
-app.get('/article-three',function(req,res){
-    
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
