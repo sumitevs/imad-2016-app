0e1a27ed55cb40efcd608f714744f202461079f3
sumitevs <sumitevs@gmail.com>
2016-09-30 00:07:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,3 @@
 console.log('Loaded!');
 
-var img = document.getElementById('pici');
-var marginLeft = 0;
-function moveLeft(){
-    magrinLeft = marginLeft +50;
-    img.style.marginLeft = marginLeft + 'px';
-}
 
-img.onclick = function(){
-    var interval = setInterval(moveLeft,50);
-}
